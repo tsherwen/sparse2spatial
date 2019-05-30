4adb2d3877566ceb1e1bb6b9889a25edb8a70e4d
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2019-05-30 12:31:32 +0100

    Updates to follow PEP8 (non aggressively) - HC2

--- scripts/CHBr3/observations.py
+++ scripts/CHBr3/observations.py
@@ -21,7 +21,7 @@ def get_CHBr3_obs(target='CHBr3', limit_depth_to=20,):
     # Where is the file?
     s2s_root = utils.get_file_locations('s2s_root')
     folder = '{}/{}/inputs/'.format(s2s_root, target)
-    df = pd.read_csv( folder+filename )
+    df = pd.read_csv(folder+filename)
     # Variable name? - Just use one of the values for now
     Varname = 'CHBr3 (pM)'
     # Assume using coord variables for now
@@ -31,8 +31,10 @@ def get_CHBr3_obs(target='CHBr3', limit_depth_to=20,):
     TimeVar1 = 'Date (UTC) and time'
     TimeVar2 = 'Sampling date/time (UT)'
     month_var = 'Month'
-    dt = pd.to_datetime(df[TimeVar1], format='%Y-%m-%d %H:%M:%S', errors='coerce' )
+    dt = pd.to_datetime(
+        df[TimeVar1], format='%Y-%m-%d %H:%M:%S', errors='coerce')
     df['datetime'] = dt
+
     def get_month(x):
         return x.month
     df[month_var] = df['datetime'].map(get_month)
@@ -40,26 +42,27 @@ def get_CHBr3_obs(target='CHBr3', limit_depth_to=20,):
     for var in [Varname]+[LatVar1, LonVar1]:
         df.loc[:, var] = pd.to_numeric(df[var].values, errors='coerce')
         # replace flagged values with NaN
-        df.replace(999,np.NaN, inplace=True)
-        df.replace(-999,np.NaN, inplace=True)
+        df.replace(999, np.NaN, inplace=True)
+        df.replace(-999, np.NaN, inplace=True)
     # Update names to use
-    cols2use = ['datetime', 'Month', LatVar1, LonVar1, Varname ]
+    cols2use = ['datetime', 'Month', LatVar1, LonVar1, Varname]
     name_dict = {
-    LatVar1: 'Latitude', LonVar1: 'Longitude', month_var: 'Month', Varname: target
+        LatVar1: 'Latitude', LonVar1: 'Longitude', month_var: 'Month', Varname: target
     }
     df = df[cols2use].rename(columns=name_dict)
     # Add a unique identifier
-    df['NEW_INDEX'] = range(1, df.shape[0]+1 )
+    df['NEW_INDEX'] = range(1, df.shape[0]+1)
     # Kludge for now to just a name then number
+
     def get_unique_Data_Key_ID(x):
-        return 'HC_{:0>6}'.format( int(x) )
+        return 'HC_{:0>6}'.format(int(x))
     df['Data_Key_ID'] = df['NEW_INDEX'].map(get_unique_Data_Key_ID)
     # Remove all the NaNs
     t0_shape = df.shape[0]
     df = df.dropna()
     if t0_shape != df.shape[0]:
         pstr = 'WARNING: Dropped obs. (#={}), now have #={} (had #={})'
-        print( pstr.format(t0_shape-df.shape[0], df.shape[0], t0_shape) )
+        print(pstr.format(t0_shape-df.shape[0], df.shape[0], t0_shape))
     return df
 
 
@@ -151,15 +154,14 @@ def get_processed_df_obs_mod(reprocess_params=False, target='CHBr3',
     return df
 
 
-
 def add_extra_vars_rm_some_data(df=None, target='CHBr3',
                                 restrict_data_max=False, restrict_min_salinity=False,
-#                                use_median_value_for_chlor_when_NaN=False,
-#                                median_4MLD_when_NaN_or_less_than_0=False,
-#                                median_4depth_when_greater_than_0=False,
+                                #                                use_median_value_for_chlor_when_NaN=False,
+                                #                                median_4MLD_when_NaN_or_less_than_0=False,
+                                #                                median_4depth_when_greater_than_0=False,
                                 rm_LOD_filled_data=False,
-#                                add_modulus_of_lat=False,
-#                                rm_Skagerrak_data=False,
+                                #                                add_modulus_of_lat=False,
+                                #                                rm_Skagerrak_data=False,
                                 rm_outliers=False, verbose=True, debug=False):
     """
     Add, process, or remove (requested) derivative variables for use with ML code
@@ -176,7 +178,8 @@ def add_extra_vars_rm_some_data(df=None, target='CHBr3',
     N0 = df.shape[0]
     # remove the outlier values
     if rm_outliers:
-        Outlier = utils.get_outlier_value(df, var2use=target, check_full_df_used=False)
+        Outlier = utils.get_outlier_value(
+            df, var2use=target, check_full_df_used=False)
         bool = df[target] < Outlier
         df_tmp = df.loc[bool]
         prt_str = 'Removing outlier {} values. (df {}=>{},{})'
