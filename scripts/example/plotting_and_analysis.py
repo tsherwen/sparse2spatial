8d95b3f79129d798d4529d2b0cf7f1c87c69f0f0
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2019-05-30 14:25:59 +0100

    Inc. example annual mean plot for spatial surface prediction

---
+++ scripts/example/plotting_and_analysis.py
@@ -0,0 +1,29 @@
+"""
+Plotting and analysis code for sea-surface example prediction work.
+"""
+import numpy as np
+import xarray as xr
+import pandas as pd
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+import sparse2spatial as s2s
+import sparse2spatial.utils as utils
+import sparse2spatial.plotting as plotting
+
+def main():
+    """
+    Driver to do plotting/analysis for example
+    """
+    # - Local variables
+    target = 'example'
+    # - Explore the predicted concentrations
+    # Get the data
+    ds = get_predicted_3D_values(target=target)
+    # plot up an annual mean
+    plotting.plot_up_annual_averages_of_prediction(ds=ds, target=target)
+
+
+if __name__ == "__main__":
+    main()
