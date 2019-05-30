53beeabd6c1c97534cc311d80b9aa6053b121ccc
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2019-05-30 12:40:32 +0100

    Update variable/func. names on CH3I branch

--- scripts/CH3I/sea_surface_CH3I.py
+++ scripts/CH3I/sea_surface_CH3I.py
@@ -18,10 +18,10 @@ import sparse2spatial as s2s
 import sparse2spatial.utils as utils
 #import sparse2spatial.ancillaries2grid_oversample as ancillaries2grid
 #import sparse2spatial.archiving as archiving
-from sparse2spatial.RFRbuild import mk_ML_testing_and_training_set
+from sparse2spatial.RFRbuild import mk_testing_training_sets
 import sparse2spatial.RFRbuild as build
 import sparse2spatial.RFRanalysis as analysis
-from sparse2spatial.RFRbuild import build_or_get_current_models
+from sparse2spatial.RFRbuild import build_or_get_models
 
 # Get iodide specific functions
 
@@ -38,9 +38,9 @@ def main():
 #    df = get_dataset_processed4ML(target=target, rm_outliers=rm_outliers)
 
     # - build models with the observations
-#    RFR_dict = build_or_get_current_models_CH3I(rebuild=False, target=target)
+#    RFR_dict = build_or_get_models_CH3I(rebuild=False, target=target)
     # build the models (just run once!)
-    RFR_dict = build_or_get_current_models_CH3I(rebuild=True, target=target)
+    RFR_dict = build_or_get_models_CH3I(rebuild=True, target=target)
 
     # Get stats ont these models
     stats = analysis.get_core_stats_on_current_models(RFR_dict=RFR_dict,
@@ -58,35 +58,35 @@ def main():
     # Resolution to use? (full='0.125x0.125', test at lower e.g. '4x5')
 #    res = '0.125x0.125'
     res='4x5'
-    build.mk_predictions_from_ancillaries(None, res=res, RFR_dict=RFR_dict,
+    build.mk_predictions_for_3D_features(None, res=res, RFR_dict=RFR_dict,
                                     save2NetCDF=save2NetCDF, target=target,
                                     models2compare=topmodels,
                                     topmodels=topmodels,
                                     xsave_str=xsave_str, add_ensemble2ds=True )
 
 
-def build_or_get_current_models_CH3I(rm_Skagerrak_data=True, target='CH3I',
+def build_or_get_models_CH3I(rm_Skagerrak_data=True, target='CH3I',
                                        rm_LOD_filled_data=False,
                                        rm_outliers=True,
                                        rebuild=False ):
     """
-    Wrapper call to build_or_get_current_models for sea-surface CH3I
+    Wrapper call to build_or_get_models for sea-surface CH3I
     """
     # Get the dictionary  of model names and features (specific to iodide)
-    model_feature_dict = utils.get_model_testing_features_dict(rtn_dict=True)
+    model_feature_dict = utils.get_model_features_used_dict(rtn_dict=True)
 
     # Get the observational dataset prepared for ML pipeline
     df = get_dataset_processed4ML(target=target, rm_outliers=rm_outliers)
 
     if rebuild:
-        RFR_dict = build_or_get_current_models(save_model_to_disk=True,
+        RFR_dict = build_or_get_models(save_model_to_disk=True,
 #                                    rm_Skagerrak_data=rm_Skagerrak_data,
                                     model_feature_dict=model_feature_dict,
                                     df=df, target=target,
                                     read_model_from_disk=False,
                                     delete_existing_model_files=True )
     else:
-        RFR_dict = build_or_get_current_models(save_model_to_disk=False,
+        RFR_dict = build_or_get_models(save_model_to_disk=False,
 #                                    rm_Skagerrak_data=rm_Skagerrak_data,
                                     model_feature_dict=model_feature_dict,
                                     df=df, target=target,
@@ -118,7 +118,7 @@ def get_dataset_processed4ML(restrict_data_max=False, target='CH3I',
     from observations import add_extra_vars_rm_some_data
     from observations import get_processed_df_obs_mod
     # - Local variables
-    testing_features = None
+    features_used = None
     target = 'CH3I'
     target_name = [target]
     # - The following settings are set to False as default
@@ -145,7 +145,7 @@ def get_dataset_processed4ML(restrict_data_max=False, target='CH3I',
     print('WARNING: Reindexed to shape of DataFrame processed for ML ({})'.format(N1))
 
     # - Add test and training set assignment to columns
-    # standard split vars?  (values=random_20_80_split, random_strat_split)
+    # standard split vars?  (values=rand_20_80, rand_strat)
     ways2split_data = {
         'rn. 20%': (True, False),
         'strat. 20%': (False, True),
@@ -153,15 +153,15 @@ def get_dataset_processed4ML(restrict_data_max=False, target='CH3I',
     # Loop training/test split methods
     for key_ in ways2split_data.keys():
         # Get settings
-        random_20_80_split, random_strat_split = ways2split_data[key_]
+        rand_20_80, rand_strat = ways2split_data[key_]
         # Copy a df for splitting
 #        df_tmp = df['Iodide'].copy()
         # Now split using existing function
-        returned_vars = mk_ML_testing_and_training_set(df=df.copy(),
+        returned_vars = mk_testing_training_sets(df=df.copy(),
                                                     target=target,
-                                                    random_20_80_split=random_20_80_split,
-                                                    random_strat_split=random_strat_split,
-                                                    testing_features=df.columns.tolist(),
+                                                    rand_20_80=rand_20_80,
+                                                    rand_strat=rand_strat,
+                                                    features_used=df.columns.tolist(),
                                                               )
         train_set, test_set, test_set_targets = returned_vars
         # Now assign the values
