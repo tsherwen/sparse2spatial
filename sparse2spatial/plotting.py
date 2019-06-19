bc6536d6f2553f9f028bdf1ab289d5f050e67756
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2019-06-19 16:03:18 +0100

    Add a basic seasonal plotter for the data

--- sparse2spatial/plotting.py
+++ sparse2spatial/plotting.py
@@ -26,7 +26,7 @@ def plot_up_annual_averages_of_prediction(ds=None, target=None, version='v0_0_0'
 
     Parameters
     -------
-    ds (xr.dataset), 3D dataset contraining variable of interest on monthly basis 
+    ds (xr.dataset), 3D dataset contraining variable of interest on monthly basis
     target (str), Name of the target variable (e.g. iodide)
     version (str), Version number or string (present in NetCDF names etc)
 
@@ -44,30 +44,73 @@ def plot_up_annual_averages_of_prediction(ds=None, target=None, version='v0_0_0'
         title=title)
 
 
+def plot_up_seasonal_averages_of_prediction(ds=None, target=None, version='v0_0_0',
+        seperate_plots=True, verbose=False ):
+    """
+    Wrapper to plot up the annual averages of the predictions
+
+    Parameters
+    -------
+    ds (xr.dataset), 3D dataset contraining variable of interest on monthly basis
+    target (str), Name of the target variable (e.g. iodide)
+    version (str), Version number or string (present in NetCDF names etc)
+
+    Returns
+    -------
+    (None)
+    """
+    # Get annual average
+    var2plot = 'Ensemble_Monthly_mean'
+    # Get average by season
+    ds = ds.groupby('time.season').mean(dim='time')
+    # Plot by season
+    if seperate_plots:
+        for season in list(ds.season.values):
+            # check and name variables
+            extr_str = '{}_{}'.format(version, season)
+            if verbose:
+                print( season, extr_str )
+            # Select data for month
+            ds2plot = ds[[var2plot]].sel(season=season)
+            # Set a title
+            title = "Seasonal ({}) average ensemble prediction for '{}' (pM)"
+            title = title.format(season, target)
+            # Now plot
+            plot_spatial_data(ds=ds2plot, var2plot=var2plot, extr_str=extr_str,
+                target=target, title=title)
+    # Or plot up as a window plot
+    else:
+        print('TODO: setup to plot window plot by season')
+
+
 def plot_spatial_data(ds=None, var2plot=None, LatVar='lat', LonVar='lon',
                       extr_str='', fillcontinents=True, target=None, units=None,
                       show_plot=False, save_plot=True, title=None,
-                      dpi=320):
+                      projection=ccrs.Robinson(), fig=None, ax=None,
+                      vmin=None, vmax=None, dpi=320):
     """
     Plot up 2D spatial plot of latitude vs. longitude
 
-    Parameters                                                                                 
-    -------                                                                                    
-    ds (xr.dataset), 3D dataset contraining variable of interest on monthly basis              
-    var2plot (str), variable to plot from dataset
-    target (str), Name of the target variable (e.g. iodide)                                    
-    version (str), Version number or string (present in NetCDF names etc)                      
-    file_and_path (str), folder and filename with location settings as single str              
-    res (str), horizontal resolution of dataset (e.g. 4x5)                                     
-                                                                                               
-    Returns                                                                                    
+    Parameters
     -------
-    (None)  
+    ds (xr.dataset), 3D dataset contraining variable of interest on monthly basis
+    var2plot (str), variable to plot from dataset
+    target (str), Name of the target variable (e.g. iodide)
+    version (str), Version number or string (present in NetCDF names etc)
+    file_and_path (str), folder and filename with location settings as single str
+    res (str), horizontal resolution of dataset (e.g. 4x5)
+
+    Returns
+    -------
+    (None)
     """
     import cartopy.crs as ccrs
-    fig = plt.figure(figsize=(10, 6))
-    ax = fig.add_subplot(111, projection=ccrs.Robinson(), aspect='auto')
-    ds[var2plot].plot.imshow(x='lon', y='lat', ax=ax, transform=ccrs.PlateCarree())
+    if isinstance(fig, type(None)):
+        fig = plt.figure(figsize=(10, 6))
+    if isinstance(ax, type(None)):
+        ax = fig.add_subplot(111, projection=projection, aspect='auto')
+    ds[var2plot].plot.imshow(x='lon', y='lat', ax=ax, vmax=vmax, vmin=vmin,
+        transform=ccrs.PlateCarree())
     # Fill the continents
     if fillcontinents:
         ax.add_feature(cartopy.feature.LAND, zorder=100, edgecolor='k')
